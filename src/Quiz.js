c7a3d5407b457604ea4ad07984e1be1e8114bcfe
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2023-02-18 20:35:17 +0530

    module 1 fix

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 
 let quizData = require('./quiz_data.json')
 
-export default class Quiz extends Component {
-	constructor() {
-		super()
+class Quiz extends Component {
+	constructor(props) {
+		super(props)
 
 		this.state = { quiz_position: 1 }
 	}
@@ -17,3 +17,5 @@ export default class Quiz extends Component {
 		)
 	}
 }
+
+export default Quiz
