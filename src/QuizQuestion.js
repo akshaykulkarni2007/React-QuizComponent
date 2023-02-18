516dbff4af0a8bacf5ac222f5387053b9c5c7b3e
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2023-02-18 22:58:51 +0530

    module 2 fix

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -6,12 +6,11 @@ class QuizQuestion extends Component {
 			<main>
 				<section>
 					<p>{this.props.quiz_question.instruction_text}</p>
+				</section>
+				<section className="buttons">
 					<ul>
 						<li>{this.props.quiz_question.answer_options[0]}</li>
 					</ul>
-				</section>
-				<section className="buttons">
-					<ul>//quiz question button logic goes here</ul>
 				</section>
 			</main>
 		)
